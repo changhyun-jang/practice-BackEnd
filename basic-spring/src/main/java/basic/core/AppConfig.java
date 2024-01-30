3c89f73f753d32dfc261612a1a89e7e517a97520
changhyun-jang <ckdgusjang@naver.com>
2024-01-30 20:40:00 +0900

    24_01_30 Appconfig refactor를 통한 중복제거

--- basic-spring/src/main/java/basic/core/AppConfig.java
+++ basic-spring/src/main/java/basic/core/AppConfig.java
@@ -1,6 +1,8 @@
 package basic.core;
 
+import basic.core.discount.DiscountPolicy;
 import basic.core.discount.FixDiscountPolicy;
+import basic.core.member.MemberRepository;
 import basic.core.member.MemberService;
 import basic.core.member.MemberServiceImpl;
 import basic.core.member.MemoryMemberRepository;
@@ -8,13 +10,20 @@ import basic.core.order.OrderService;
 import basic.core.order.OrderServiceImpl;
 
 public class AppConfig {
+    //역할을 세우고 그안에 구현이 어떻게 되어있는지 보이도록 구현, 중복 제거
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
 }
