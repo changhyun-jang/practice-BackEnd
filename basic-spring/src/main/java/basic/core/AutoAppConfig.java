621b8eeae221b1d0b40a91c465db971d4983aa42
changhyun-jang <ckdgusjang@naver.com>
2024-02-04 10:28:08 +0900

    24_02_04 스캔대상 설정 및 필터

--- basic-spring/src/main/java/basic/core/AutoAppConfig.java
+++ basic-spring/src/main/java/basic/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        //지정하지 않으면 @ComponentScan이 붙은 설정 정보 클래스의 시작 위치가 시작
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
