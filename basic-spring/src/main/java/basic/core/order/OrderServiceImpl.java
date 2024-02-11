322adc3e7b7f9dfb1a786f554cf9b91febbccfce
changhyun-jang <ckdgusjang@naver.com>
2024-02-11 17:52:47 +0900

    24_02_11 Lombok 라이브러리 사용한 코드 최적화

--- basic-spring/src/main/java/basic/core/order/OrderServiceImpl.java
+++ basic-spring/src/main/java/basic/core/order/OrderServiceImpl.java
@@ -3,46 +3,18 @@ package basic.core.order;
 import basic.core.discount.DiscountPolicy;
 import basic.core.member.Member;
 import basic.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
+//final이 붙은애들로 생성자 생성
 public class OrderServiceImpl implements OrderService {
-    //    private MemberRepository memberRepository;
     
     //생성자 주입시 final사용가능
     private final MemberRepository memberRepository;
-    //    private DiscountPolicy discountPolicy;
     private final DiscountPolicy discountPolicy;
-
-    //setter주입
-//    @Autowired
-//    public void setMemberRepository(MemberRepository memberRepository) {
-//        this.memberRepository = memberRepository;
-//    }
-//    @Autowired
-//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-//        this.discountPolicy = discountPolicy;
-//    }
-
-    // 생성자 주입
-    // new OrderServiceImple(membreRepository, discountPolicy);
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-
-    //OCP원칙 위반
-    //DIP원칙 위반
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-
-//    @Autowired
-//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -56,4 +28,34 @@ public class OrderServiceImpl implements OrderService {
     public MemberRepository getMemberRepository() {
         return memberRepository;
     }
+
+    //setter주입
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
+    // 생성자 주입
+    // new OrderServiceImple(membreRepository, discountPolicy);
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
+
+    //OCP원칙 위반
+    //DIP원칙 위반
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+//    @Autowired
+//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
+
+
 }
