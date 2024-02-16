ea33ff381582691cf1c101afeca7616249b7d861
JCH <ckdgusjang@naver.com>
2024-02-16 22:12:39 +0900

    24_02_16 Bag캡슐화

--- OOP/src/main/java/OOP/core/ticket/Audience.java
+++ OOP/src/main/java/OOP/core/ticket/Audience.java
@@ -13,13 +13,15 @@ public class Audience {
 //    }
 
     public Long buy(Ticket ticket){
-        if(bag.hasInvitation()){
-            bag.setTicket(ticket);
-            return 0L;
-        } else {
-            bag.setTicket(ticket);
-            bag.minusAmount(ticket.getFee());
-            return ticket.getFee();
-        }
+//        3차코드 - Bag캡슐화
+//        if(bag.hasInvitation()){
+//            bag.setTicket(ticket);
+//            return 0L;
+//        } else {
+//            bag.setTicket(ticket);
+//            bag.minusAmount(ticket.getFee());
+//            return ticket.getFee();
+//        }
+        return bag.hold(ticket);
     }
 }
--- OOP/src/main/java/OOP/core/ticket/Bag.java
+++ OOP/src/main/java/OOP/core/ticket/Bag.java
@@ -5,7 +5,18 @@ public class Bag {
     private Invitation invitation;
     private Ticket ticket;
 
-    public boolean hasInvitation() {
+    public Long hold(Ticket ticket) {
+        if (hasInvitation()) {
+            setTicket(ticket);
+            return 0L;
+        } else {
+            setTicket(ticket);
+            minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
+
+    private boolean hasInvitation() {
         return invitation != null;
     }
 
@@ -13,11 +24,11 @@ public class Bag {
         return ticket != null;
     }
 
-    public void setTicket(Ticket ticket) {
+    private void setTicket(Ticket ticket) {
         this.ticket = ticket;
     }
 
-    public void minusAmount(Long amount) {
+    private void minusAmount(Long amount) {
         this.amount -= amount;
     }
 
