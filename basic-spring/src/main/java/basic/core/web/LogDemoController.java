c1bfdd39447211631427d4b52895b0b1f5228805
JCH <ckdgusjang@naver.com>
2024-02-16 15:48:30 +0900

    24_02_16 웹스코프 중 request스코프 예제 문제 발생 - 스프링 컨테이너 생성시 request가 없으므로 MyLogger라는 클래스의 빈이 생성이 안되어 에러 발생

---
+++ basic-spring/src/main/java/basic/core/web/LogDemoController.java
@@ -0,0 +1,28 @@
+package basic.core.web;
+
+import basic.core.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @GetMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request){
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+
+    }
+}
---
+++ basic-spring/src/main/java/basic/core/web/LogDemoService.java
@@ -0,0 +1,14 @@
+package basic.core.web;
+
+import basic.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+    private MyLogger myLogger;
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+}
