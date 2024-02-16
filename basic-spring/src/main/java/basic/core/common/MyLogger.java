d001a3d42ceae5abe064fffd6320ed42dc4d85b9
JCH <ckdgusjang@naver.com>
2024-02-16 16:28:54 +0900

    24_02_16 Proxy를 통한 코드 간소화 - Provider든 Proxy든 진짜 객체를 조회할때까지의 지연처리를 수행

--- basic-spring/src/main/java/basic/core/common/MyLogger.java
+++ basic-spring/src/main/java/basic/core/common/MyLogger.java
@@ -4,10 +4,14 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import java.util.UUID;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(value = "request")
+//프록시 설정 하려면 value가 있어야함, 대상이 클래스면 TARGET_CLASS를 인터페이스면 INTERFACE를 주면됨
+//이를 이용하면 가짜 프록시 객체를 만들어 미리 주입해둠(springCGLIB)
+//가짜 프록시객체에게 요청이 오면 실제 객체에게 위임하는 로직이 있음.
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MyLogger {
     private String uuid;
     private String requestURL;
