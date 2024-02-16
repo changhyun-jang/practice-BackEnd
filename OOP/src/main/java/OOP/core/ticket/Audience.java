ce2a5aa32a9aa13ce19eeeedbfed04fe0b40b5c6
JCH <ckdgusjang@naver.com>
2024-02-16 21:10:51 +0900

    24_02_16 Audience와 TicketSeller의 캡슐화를 통한 응집도 강화와 결합도 약화

--- OOP/src/main/java/OOP/core/ticket/Audience.java
+++ OOP/src/main/java/OOP/core/ticket/Audience.java
@@ -7,7 +7,19 @@ public class Audience {
         this.bag = bag;
     }
 
-    public Bag getBag(){
-        return bag;
+//    2차코드 - Audience 캡슐화
+//    public Bag getBag(){
+//        return bag;
+//    }
+
+    public Long buy(Ticket ticket){
+        if(bag.hasInvitation()){
+            bag.setTicket(ticket);
+            return 0L;
+        } else {
+            bag.setTicket(ticket);
+            bag.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
     }
 }
--- OOP/src/main/java/OOP/core/ticket/TicketSeller.java
+++ OOP/src/main/java/OOP/core/ticket/TicketSeller.java
@@ -6,8 +6,22 @@ public class TicketSeller {
     public TicketSeller(TicketOffice ticketOffice) {
         this.ticketOffice = ticketOffice;
     }
+//    1차 코드 - ticketSeller 캡슐화
+//    public TicketOffice getTicketOffice() {
+//        return ticketOffice;
+//    }
 
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
+    public void sellTo(Audience audience){
+//        2차코드 - Audience 캡슐화
+//        if (audience.getBag().hasInvitation()) {
+//            Ticket ticket = ticketOffice.getTicket();
+//            audience.getBag().setTicket(ticket);
+//        } else {
+//            Ticket ticket = ticketOffice.getTicket();
+//            audience.getBag().minusAmount(ticket.getFee());
+//            ticketOffice.plusAmount(ticket.getFee());
+//            audience.getBag().setTicket(ticket);
+//        }
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
     }
 }
