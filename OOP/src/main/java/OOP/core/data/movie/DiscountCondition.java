e497c806e2fb35af04eaede1591cb6b7f7656322
JCH <ckdgusjang@naver.com>
2024-02-21 20:57:48 +0900

    24_02_21 데이터 중신 설계 DiscountCondition 추가

---
+++ OOP/src/main/java/OOP/core/data/movie/DiscountCondition.java
@@ -0,0 +1,34 @@
+package OOP.core.data.movie;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+public class DiscountCondition {
+    private DiscountConditionType type;
+
+    private int sequence;
+
+    private DayOfWeek dayOfWeek;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public DiscountConditionType getType() {
+        return type;
+    }
+
+    public int getSequence() {
+        return sequence;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+}
---
+++ OOP/src/main/java/OOP/core/data/movie/DiscountConditionType.java
@@ -0,0 +1,6 @@
+package OOP.core.data.movie;
+
+public enum DiscountConditionType {
+    SEQUENCE,   //순번 조건
+    PERIOD      //기간 조건
+}
