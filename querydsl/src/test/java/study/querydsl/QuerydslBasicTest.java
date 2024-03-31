aa447b9d922c628d04420c81758350bd1158e5ef
JCH <ckdgusjang@naver.com>
2024-03-31 20:59:39 +0900

    24_03_31 결과 조회하기

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.assertj.core.api.Assertions;
@@ -11,6 +12,8 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static study.querydsl.entity.QMember.*;
@@ -77,6 +80,60 @@ public class QuerydslBasicTest {
                 .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void search() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1").and(member.age.eq(10)))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void searchAndParam() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                )
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void resultFetch() {
+        //리스트 조회, 데이터 없으면 빈 리스트 반환
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
+
+        //리스트 단건 조회, 결과가 없으면 null, 둘이사이면 에러 처리
+        Member fetchOne = queryFactory
+                .selectFrom(member)
+                .fetchOne();
+    
+        //limit 1;
+        Member fetchFirst = queryFactory
+                .selectFrom(member)
+                .fetchFirst();
+
+        //페이징 정보 포함, total count쿼리 추가 실행하여 2번 쿼리가 실행
+        //성능이 중요한 부분에서는 이것을 사용하지 않기
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+        List<Member> content = results.getResults();
+
+        //count쿼리로 변경
+        long total = queryFactory
+                .selectFrom(member)
+                .fetchCount();
 
 
     }
